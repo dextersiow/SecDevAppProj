80a5c506153057e6063a76577f909de344df72f6
dextersiow <dextersiow1@gmail.com>
2023-01-31 22:37:45 +0000

    create functions to use on the app

--- functions.php
+++ functions.php
@@ -1,19 +1,38 @@
 <?php
+ require_once "connection.php";
 function filter(){
-
+    //XSS filter
 }
 
-function authenticate(){
-
+function authenticate($username, $password){
+    $query = "SELECT * FROM users WHERE username='$username'";
+    $result = $conn->query($query);
+    if ($result->num_rows == 0) {
+        return false;
+      } else {
+        $user = $result->fetch_assoc();
+        $hashSalt = $user['salt'];
+        $saltedHashedPassword = hash("sha256", $hashSalt . $password);
+    
+        if ($saltedHashedPassword == $user['password']) {
+          return true;
+        } else {
+          return false;
+        }
+      } 
+}
+function authenticateSession($session){
+    //check authenticated active session 
 }
 
 function logout(){
-
+    //logout user
 }
 
 function validatePassword(){
-
+    //validate password on creation
 }
+
 
 
 ?>
