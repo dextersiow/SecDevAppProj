67f4c490b0541eaff494c9b3bc7444d028887196
dextersiow <dextersiow1@gmail.com>
2023-02-09 11:50:47 +0000

    clean up login page

--- login.php
+++ login.php
@@ -1,35 +1,21 @@
 <?php 
-require_once "connection.php";
+session_start();
+require_once "workingconnection.php";
+require_once "functions.php";
 
 if (isset($_POST['username']) && isset($_POST['password'])) {
   $username = $_POST['username'];
   $password = $_POST['password'];
 
-  $query = "SELECT * FROM users WHERE username=?";
-  $stmt = $conn->prepare($query);
-  $stmt->bind_param("s", $username);
-  $stmt->execute();
-  $result = $stmt->get_result();
-
-  if ($result->num_rows == 0) {
-    echo "Username and/or password is incorrect.<br>";
-  } else {
-    $user = $result->fetch_assoc();
-    $hashSalt = $user['salt'];
-    $saltedHashedPassword = hash("sha256", $hashSalt . $password);
-
-    if ($saltedHashedPassword == $user['password']) {
-	session_start();
-	$username = $_SESSION['username'];
-	$key = "YourEncryptionKey";
-	$encryptedUsername = base64_encode(openssl_encrypt($username, 'AES-128-ECB', $key));
-	$_SESSION['encrypted_username'] = $encryptedUsername;
-      echo "Login successful!<br>";
+  //authenticate user
+  if(authenticate($username,$password)){
+    session_id(session_create_id());
+    echo "Login successful!<br>";
 	  header("Location: authpage1.php");
-    } else {
-      echo "The username ". $username . "and password could not be authenticated at the moment. <br>";
-    }
+  } else {
+    echo "The username ". $username . "and password could not be authenticated at the moment. <br>";
   }
+
 } else {
   echo "Please enter a username and password.<br>";
 }
@@ -53,8 +39,6 @@ $conn->close();
     <form class="form-signin" action="" method="post">
         <a href="index.php"><img class="mb-4" src="img/logo.png" width="300" height="180"></a>
         <h1 class="h3 mb-3 font-weight-normal">Sign in</h1>
-
-        <?php //if(isset($username_err))echo $Username_err ?>
 
         <label for="inputUsername" class="sr-only">Username</label>
         <input type="text" name="username" id="inputUsername" class="form-control" placeholder="Username" required autofocus>
