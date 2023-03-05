4f0d842c13159e4952eeb91ce9ae8f8f871a6225
jasondarley <72496340+jasondarley@users.noreply.github.com>
2023-03-05 20:18:24 +0000

    Update authpage1.php
    after 20 seconds of inactivity a redirect to login.php will occur will change timer to 10 minutes just using 20 seconds to make it easier to test for now

--- authpage1.php
+++ authpage1.php
@@ -1,38 +1,62 @@
 <?php
-	session_start();
-	$encryptedUsername = $_SESSION['encrypted_username'];
-	$key = "YourEncryptionKey";
-	$username = openssl_decrypt(base64_decode($encryptedUsername), 'AES-128-ECB', $key);
-	echo "Welcome, " . $username;
-
-     $pageTitle = "My Web Page";
+    $pageTitle = "My Web Page";
     $pageContent = "Welcome to my web page! This is a simple PHP generated page.";
     $headerText = "My Web Page";
     $headerColor = "#000000";
     $headerSize = "36px";
     $headerFont = "Helvetica";
-    
-    echo "<html>";
-    echo "<head>";
-    echo "<title>".$pageTitle."</title>";
-    echo "<style> 
-            h1 {
-                color: ".$headerColor.";
-                font-size: ".$headerSize.";
-                font-family: ".$headerFont.";
-                text-align: center;
-            }
-            p {
-                text-align: center;
-                margin: 20px;
-                padding: 20px;
-                font-size: 18px;
-            }
-            </style>";
-    echo "</head>";
-    echo "<body>";
-    echo "<h1>".$headerText."</h1>";
-    echo "<p>".$pageContent."</p>";
-    echo "</body>";
-    echo "</html>";
 ?>
+
+<html>
+<head>
+    <title><?php echo $pageTitle; ?></title>
+    <style> 
+        h1 {
+            color: <?php echo $headerColor; ?>;
+            font-size: <?php echo $headerSize; ?>;
+            font-family: <?php echo $headerFont; ?>;
+            text-align: center;
+        }
+        p {
+            text-align: center;
+            margin: 20px;
+            padding: 20px;
+            font-size: 18px;
+        }
+    </style>
+</head>
+
+<body>
+    <h1><?php echo $headerText; ?></h1>
+    <p><?php echo $pageContent; ?></p>
+
+    <script>
+        // set timeout function
+        var timeout;
+
+        // start timer function
+        function startTimer() {
+            timeout = setTimeout(redirect, 20000); // 20 seconds in milliseconds
+        }
+
+        // reset timer function
+        function resetTimer() {
+            clearTimeout(timeout);
+            startTimer();
+        }
+
+        // redirect function
+        function redirect() {
+            window.location.href = "login.php";
+        }
+
+        // start the timer on page load
+        window.onload = startTimer;
+
+        // reset the timer on mouse move or keyboard activity
+        document.onmousemove = resetTimer;
+        document.onkeydown = resetTimer;
+    </script>
+</body>
+</html>
+
